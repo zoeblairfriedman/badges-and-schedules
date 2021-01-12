57bcfc4f6af644bedace7ac5b40e3e425975bb30
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-12 18:07:55 -0500

    commit tree f15e55beb87d4b61e885d3d0a9092362bac15cc0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+    badges = Array.new
+    array_of_names.each do |name_in_array|
+        badges << badge_maker(name_in_array)
+    end
+    return badges
+end
+
+def assign_rooms(attendees)
+    attendee_messages = Array.new
+    attendees.each.with_index(1) do |name, index|
+        attendee_messages << "Hello, #{name}! You'll be assigned to room #{index}!"
+      end
+    return attendee_messages
+ end
+
+ def printer(input)
+    badges = batch_badge_creator(input)
+    room_assignments = assign_rooms(input)
+    
+    badges.each do |badge|
+      puts badge
+    end
+
+    room_assignments.each do |assignment|
+      puts assignment
+    end
+ end
